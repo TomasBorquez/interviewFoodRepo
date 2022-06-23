79e1a368bcc1b574faba8fbf6cf89141180bd818
Tomas <tomasborquez13@gmail.com>
2022-06-23 14:52:01 -0300

    database stuff finished i think..

--- api/src/middlewares/recipes.js
+++ api/src/middlewares/recipes.js
@@ -1,18 +1,41 @@
+// Our modules
 const { Router } = require('express');
-const { Recipe } = require('../db');
+const { Recipe, Diet } = require('../db');
+// Libraries and such
+const axios = require('axios');
 const router = Router();
 
 // Requests get
+router.get('/:idRecipe', async (req, res) => {
+  const { idRecipe } = req.params;
+  try {
+    if (idRecipe) {
+      // const response = await Recipe.findOne({ where: { recipe_id: idRecipe } });
+      const response = await Recipe.findByPk(idRecipe, {
+        include: {
+          model: Diet,
+        },
+      });
+      if (!response) res.status(404).send('Such recipe does not exist');
+      else res.status(201).send(response);
+    } else {
+      res.status(404).send('No recipe id recieved');
+    }
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
 router.get('/', async (req, res) => {
-  const { name } = req.query; // Human Elf Demon...
+  const { name } = req.query;
   try {
     if (name) {
-      const response = await Recipe.findAll({ where: {name} });
-      if (!response.length) res.status(404).send("No recipes in the data base");
+      const response = await Recipe.findAll({ where: { name } });
+      if (!response.length) res.status(404).send('No recipes in the data base');
       else res.status(201).send(response);
     } else {
       const response = await Recipe.findAll();
-      if (!response.length) res.status(404).send("No recipes in the data base");
+      if (!response.length) res.status(404).send('No recipes in the data base');
       else res.status(201).send(response);
     }
   } catch (error) {
@@ -32,14 +55,14 @@ router.post('/', async (req, res) => {
         health_score,
         steps,
       });
-      const c = await recipe.addDiets([1, 3, 7]);
-      console.log(c)
-      // console.log("this is the proto", recipe.__proto__);
-      // res.status(201).send(`Your recipe ${name} was correctly created :D`);
-      const a = await recipe.getDiets()
-      res.status(201).send(a);
+      await axios.get("http://localhost:3001/diets");
+      if (diets) {
+        await recipe.addDiets(diets);
+        const united = await recipe.getDiets();
+        res.status(201).send(united);
+      } else res.status(201).send(`${name} has been correctly created`);
     } catch (error) {
-      res.status(404).send("There was an error" + error);
+      res.status(404).send(`There was an error ${error}`);
     }
   } else {
     res.status(404).send(`There was an error`);
@@ -47,6 +70,3 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
-
-
-// const a = await recipe.getDiets()
